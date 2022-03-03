a10d094da931416eac83ed91fd9c972789d2f92e
Chetana Hanmant Nirmal <chn3060@gmail.com>
2022-03-03 05:04:47 -0500

    changes

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,33 +1,17 @@
-import Reacr, { useState } from "react";
+import React from 'react';
 
-// component is javascrip function in react
+import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
+import './ExpenseItem.css';
 
-//indentation - Shift + Option + F
-import ExpenseDate from "./ExpenseDate";
-import "./ExpenseItem.css";
-import Card from "../UI/Card";
-
-//Arrow function
-//const ExpenseItem = (props) =>{
-// }
-
-function ExpenseItem(props) {
-  //First current state value and then new value
-  const [title, setTitle] = useState(props.title); // Hooks -> only called inside react function and cannot called inside nested functions
-
-  const clickHandlers = () => {
-    setTitle("Updated"); // Assign new value
-    console.log("updated");
-  };
-
+const ExpenseItem = (props) => {
   return (
-    <Card className="expense-item">
-      <ExpenseDate date={props.date}></ExpenseDate>
-      <div className="expense-item__description">
-        <h2>{title}</h2>
-        <div className="expense-item__price">${props.amount}</div>
+    <Card className='expense-item'>
+      <ExpenseDate date={props.date} />
+      <div className='expense-item__description'>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
       </div>
-      <button onClick={clickHandlers}></button>
     </Card>
   );
 }
